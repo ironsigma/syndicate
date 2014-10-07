ee6dba3318b48198d8acc06fc43748b0fd3f7c71
Juan D Frias <juandfrias@gmail.com>
2014-10-07 10:45:45 -0700

    Switching to Java implementation

--- src/Cyniris/SyndicateBundle/CynirisSyndicateBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Cyniris\SyndicateBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class CynirisSyndicateBundle extends Bundle
-{
-}
--- src/IzyLab/FeedParserBundle/IzyLabFeedParserBundle.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace IzyLab\FeedParserBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class IzyLabFeedParserBundle extends Bundle {
-}
